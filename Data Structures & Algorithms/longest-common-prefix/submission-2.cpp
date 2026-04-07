e3d3540236ae36fd6c9413e4b1b960ebacb1664b
Rishit Dutta <96435244+kabs-d@users.noreply.github.com>
2026-04-07 12:31:33 +0530

    Add: longest-common-prefix - submission-2

---
+++ Data Structures & Algorithms/longest-common-prefix/submission-2.cpp
@@ -0,0 +1,15 @@
+class Solution {
+public:
+    string longestCommonPrefix(vector<string>& strs) {
+        string prefix=strs[0];
+        for(string s: strs){
+            if(s=="") return "";
+            while(prefix.size()){
+                if(s.find(prefix)==string::npos) prefix.pop_back();
+                else break;
+            }
+            if(!prefix.size()) return "";
+        }
+        return prefix;
+    }
+};
