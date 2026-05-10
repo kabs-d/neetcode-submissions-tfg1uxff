adb44777536c86a0a587989651f7b01f5835068c
Rishit Dutta <96435244+kabs-d@users.noreply.github.com>
2026-05-10 11:10:05 +0530

    Add: buy-and-sell-crypto - submission-1

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-1.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int profit=0;
+        int cost=prices[0];
+        for(int i=0; i<prices.size(); i++){
+            cost=min(cost, prices[i]);
+            profit=max(profit, prices[i]-cost);
+        }
+        return profit;
+    }
+};
